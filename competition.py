ba7b8c28faff7a9e3377b43232955fd719a17e9a
Máni Bernharðsson <manib@hey.com>
2023-11-02 08:30:55 +0000

    play N double-rounds

--- competition.py
+++ competition.py
@@ -1,3 +1,4 @@
+import argparse
 from dataclasses import dataclass
 import importlib.machinery
 from itertools import permutations
@@ -7,6 +8,12 @@ import types
 import pandas as pd
 
 from mancala.game import game as play_game, ActionFunction
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-N', '--number-of-rounds', default=10, help="How many double-rounds to play.", type=int)
+args = parser.parse_args()
+N = args.number_of_rounds
 
 
 @dataclass
@@ -33,11 +40,16 @@ for group in group_dirs:
 
 
 # Let everybody play against everbody (two rounds, home and away).
-matches = permutations(groups, 2)
-results = []
-for home, away in matches:
-    result = play_game(home.action, away.action)
-    results.append((home.name, away.name, result))
+matches = tuple(permutations(groups, 2))
+index = pd.MultiIndex.from_tuples((p0.name, p1.name) for p0, p1 in matches)
+columns=[0, 1, -1]
+results = pd.DataFrame(0, index=index, columns=columns)
 
-result_df = pd.DataFrame(results, columns=['home', 'away', 'result'])
-print(result_df)
+for i in range(N):
+    for player0, player1 in matches:
+        result = play_game(player0.action, player1.action)
+        results.loc[(player0.name, player1.name), result] += 1
+
+
+results.columns = ['player0', 'player1', 'draw']
+print(results)
