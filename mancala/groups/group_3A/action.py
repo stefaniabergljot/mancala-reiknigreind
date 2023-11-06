f084e70f4a27d25a28fc99547ef81597c37ccab6
Tarnarsson <122287863+Tarnarsson@users.noreply.github.com>
2023-11-06 06:56:16 +0000

    Update action_py

--- mancala/groups/group_3A/action.py
+++ mancala/groups/group_3A/action.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import math
 
 def action(state, legalactions, player):
     
@@ -7,14 +8,18 @@ def action(state, legalactions, player):
     action = 0
     
     # Load the model from 'model.pth'
-    model = torch.load('model.pth')
+    model = torch.load('model.pth3')
     model.eval()
 
     # Use the model to compute the state and action
     q_values = model(torch.from_numpy(np.array(state)).float().unsqueeze(0)) # a tensor of shape (1, n_actions)
+    q_values = q_values.flatten().tolist() #make q's as list for easy iteration
     #for i in legalactions:
-    #    q_values = tmp_q_values[i] # to make sure we are taking a legal action, if the algorithm is right this should not have any effect.
-
-    action = q_values.max(1)[1].item() # an integer representing the action with the highest q-value
+    for i in range(6):
+        if state[i+2]>0:
+            q_values[i] = q_values[i]
+        else: q_values[i] = -math.inf
+            
+    action = q_values.index(max(q_values)) # an integer representing the action with the highest q-value
     return action
 
