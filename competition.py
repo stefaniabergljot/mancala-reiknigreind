546d48c904db001d763297307c9f41fa1daf5b3b
Máni Bernharðsson <manib@hey.com>
2023-11-23 00:44:44 +0000

    add human play and print out total score in competition.py.

--- competition.py
+++ competition.py
@@ -11,10 +11,12 @@ import pandas as pd
 from mancala.game import game as play_game, ActionFunction, NoActionException, IllegalActionException
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-N', '--number-of-rounds', default=10, help="How many double-rounds to play.", type=int)
-args = parser.parse_args()
-N = args.number_of_rounds
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-N', '--number-of-rounds', default=10, help="How many double-rounds to play.", type=int)
+    args = parser.parse_args()
+    N = args.number_of_rounds
+    return N
 
 
 @dataclass
@@ -23,77 +25,86 @@ class Group:
     action: ActionFunction
 
 
-# Get path to all groups
-p = Path('mancala/groups')
-group_dirs = [str(x) for x in p.iterdir() if x.is_dir()]
+def load_groups():
+    # Get path to all groups
+    p = Path('mancala/groups')
+    group_dirs = [str(x) for x in p.iterdir() if x.is_dir()]
 
-
-# Load each group module and assign its name and action to a Group object.
-groups = []
-for group in group_dirs:
-    group_name = group.split('/')[-1]
-    if group_name == 'example_group':
-        continue
-    loader = importlib.machinery.SourceFileLoader('group_name', group + '/action.py')
-    mod = types.ModuleType(loader.name)
-    try:
-        loader.exec_module(mod)
-    except Exception as e:
-        print(f'Excluding group {group_name}. Could not load its module:\n{e}')
-    else:
-        try:
-            name = mod.NAME
-        except Exception:
-            name = group_name
-        groups.append(Group(name, mod.action))
-
-
-# Let everybody play against everbody (two rounds, home and away).
-matches = tuple(permutations(groups, 2))
-index = pd.MultiIndex.from_tuples((p0.name, p1.name) for p0, p1 in matches)
-columns=[0, 1, -1]
-results = pd.DataFrame(0, index=index, columns=columns)
-
-
-expelled = []
-for i in range(N):
-    for player0, player1 in matches:
-        if player0 in expelled or player1 in expelled:
+    # Load each group module and assign its name and action to a Group object.
+    groups = []
+    for group in group_dirs:
+        group_name = group.split('/')[-1]
+        if group_name == 'example_group':
             continue
+        loader = importlib.machinery.SourceFileLoader('group_name', group + '/action.py')
+        mod = types.ModuleType(loader.name)
         try:
-            result = play_game(player0.action, player1.action)
-        except (NoActionException, IllegalActionException) as e:
-            player = player0 if e.player == 0 else player1
-            legal_actions = e.legal_actions
-            if isinstance(e, NoActionException):
-                print(f'Expelling {player.name} for causing exception by failing to choose action as player{e.player} when {legal_actions=}, causing the exception: {str(e)}')
-            else:
-                action = e.action
-                print(f'Expelling {player.name} for causing exception after choosing {action=} as player{e.player} when {legal_actions=} and causing the exception: {str(e)}')
-            expelled.append(player)
-            continue
+            loader.exec_module(mod)
+        except Exception as e:
+            print(f'Excluding group {group_name}. Could not load its module:\n{e}')
+        else:
+            try:
+                name = mod.NAME
+            except Exception:
+                name = group_name
+            groups.append(Group(name, mod.action))
 
-        results.loc[(player0.name, player1.name), result] += 1
+    return groups
 
 
-results.columns = ['player0', 'player1', 'draw']
-for player in expelled:
-    print(f'\nPlayer {player.name} was expelled.\n')
-print(results, '\n')
+groups = load_groups()
 
-scores = defaultdict(float)
-for index in results.index:
-    p0, p1 = index
-    draw_score = 0.5  * float(results.loc[index, 'draw'])
-    scores[p0] += float(results.loc[index, 'player0']) + draw_score
-    scores[p1] += float(results.loc[index, 'player1']) + draw_score
+if __name__ == '__main__':
+    N = cli()
+    # Let everybody play against everbody (two rounds, home and away).
+    matches = tuple(permutations(groups, 2))
+    index = pd.MultiIndex.from_tuples((p0.name, p1.name) for p0, p1 in matches)
+    columns=[0, 1, -1]
+    results = pd.DataFrame(0, index=index, columns=columns)
 
 
+    expelled = []
+    for i in range(N):
+        for player0, player1 in matches:
+            if player0 in expelled or player1 in expelled:
+                continue
+            try:
+                result = play_game(player0.action, player1.action)
+            except (NoActionException, IllegalActionException) as e:
+                player = player0 if e.player == 0 else player1
+                legal_actions = e.legal_actions
+                if isinstance(e, NoActionException):
+                    print(f'Expelling {player.name} for causing exception by failing to choose action as player{e.player} when {legal_actions=}, causing the exception: {str(e)}')
+                else:
+                    action = e.action
+                    print(f'Expelling {player.name} for causing exception after choosing {action=} as player{e.player} when {legal_actions=} and causing the exception: {str(e)}')
+                expelled.append(player)
+                continue
+
+            results.loc[(player0.name, player1.name), result] += 1
 
 
-max_length = max(len(key) for key in scores)
-print('Score'.rjust(max_length+8))
-print((max_length + 8) * '-')
+    results.columns = ['player0', 'player1', 'draw']
+    expelled_names = [e.name for e in expelled]
+    for player in expelled_names:
+        print(f'\nPlayer {player} was expelled.\n')
+    results = results.drop(expelled_names, level=0).drop(expelled_names, level=1)
 
-print('\n'.join(f'{name.ljust(max_length)}{str(score).rjust(8)}' for name, score in sorted(scores.items(), key=lambda x: x[1], reverse=True)))
-print((max_length + 8) * '-')
+    print(results.to_string(), '\n')
+
+    scores = defaultdict(float)
+    for index in results.index:
+        p0, p1 = index
+        draw_score = 0.5  * float(results.loc[index, 'draw'])
+        scores[p0] += float(results.loc[index, 'player0']) + draw_score
+        scores[p1] += float(results.loc[index, 'player1']) + draw_score
+
+
+
+
+    max_length = max(len(key) for key in scores)
+    print('Score'.rjust(max_length+8))
+    print((max_length + 8) * '-')
+
+    print('\n'.join(f'{name.ljust(max_length)}{str(score).rjust(8)}' for name, score in sorted(scores.items(), key=lambda x: x[1], reverse=True)))
+    print((max_length + 8) * '-')
---
+++ play.py
@@ -0,0 +1,38 @@
+import random
+
+from mancala.game import game as play_game
+from competition import load_groups, Group
+from mancala.groups.human.humanaction import action, board_repr
+
+
+human = Group(name='human', action=action)
+groups = load_groups()
+
+
+print('Which player do you want to play against?')
+for i, group in enumerate(groups):
+    print(f'\t{i}: {group.name}')
+print('Select number')
+group_idx = int(input())
+
+oppenent = groups[group_idx]
+_opponent_action = oppenent.action
+
+
+def oppenent_action(board, legal_actions, player):
+    print(board_repr(board, -1))
+    a = _opponent_action(board, legal_actions, player)
+    print(f'{oppenent.name} chose {a}')
+    return a
+
+
+oppenent.action = oppenent_action
+
+
+players = [human, oppenent]
+random.shuffle(players)
+
+result = play_game(players[0].action, players[1].action)
+print(f'{players[result].name} won')
+
+
